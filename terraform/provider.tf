e5b4223b6f57c0be931836549b5d61ba7c3f7edd
Aditya Dahake <97397447+adityadahake24@users.noreply.github.com>
2024-09-26 07:08:39 +0530

    Add files via upload

---
+++ terraform/provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+locals {
+  region = "ap-south-1"
+  name   = "ArgoAction"
+  vpc_cidr = "10.123.0.0/16"
+  azs      = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  tags = {
+    Example = local.name
+  }
+}
