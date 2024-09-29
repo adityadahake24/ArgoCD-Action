e505a3e09a3276c71bce17c4049fd7c8e2ff546d
Aditya Dahake <97397447+adityadahake24@users.noreply.github.com>
2024-09-29 15:42:03 +0530

    Create backend.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "autok8s-terraform-state"
+    key            = "argocd-actions/key/"
+    region         = "ap-south-1"
+    dynamodb_table = "argocd-actions-locks"
+    encrypt        = true
+  }
+}
