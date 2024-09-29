9e3813b116a6d48d1633f840b8c9ae1bcb1f3966
Aditya Dahake <97397447+adityadahake24@users.noreply.github.com>
2024-09-29 15:45:36 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "autok8s-terraform-state"
-    key            = "argocd-actions/key/"
+    key            = "argocd-actions/key"
     region         = "ap-south-1"
     dynamodb_table = "argocd-actions-locks"
     encrypt        = true
